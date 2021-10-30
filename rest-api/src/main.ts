2a788879b2437d8125da526ac52c8d7ffc3d6f2a
DanielVagner <30567979+DanielVagner@users.noreply.github.com>
2021-10-30 15:24:59 +0200

    added FallbackExceptionFilter

--- rest-api/src/main.ts
+++ rest-api/src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { FallbackExceptionFilter } from "./filter/fallback.filter";
 import { HttpExceptionFilter } from "./filter/http.filter";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.setGlobalPrefix('api');
-    app.useGlobalFilters(new HttpExceptionFilter);
+    app.useGlobalFilters(
+        new FallbackExceptionFilter(), 
+        new HttpExceptionFilter()
+        );
 
     await app.listen(9000);
 }
